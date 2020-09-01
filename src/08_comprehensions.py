a94834b73417e09942703399259fd40d9fc9a47f
Natalia <nataliamirandac@hotmail.com>
2020-09-01 19:17:17 -0400

    08_comprehensions.py

--- src/08_comprehensions.py
+++ src/08_comprehensions.py
@@ -9,8 +9,16 @@ for more info regarding list comprehensions.
 """
 
 # Write a list comprehension to produce the array [1, 2, 3, 4, 5]
-
+""" squares = []
+for x in range(10):
+...     squares.append(x**2)
+...
+ squares
+[0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+"""
 y = []
+for x in range(5):
+    y.append(x+1)
 
 print (y)
 
@@ -18,16 +26,32 @@ print (y)
 # [0, 1, 8, 27, 64, 125, 216, 343, 512, 729]
 
 y = []
+for x in range(0,10):
+    y.append(x**3)
 
 print(y)
 
 # Write a list comprehension to produce the uppercase version of all the
 # elements in array a. Hint: "foo".upper() is "FOO".
 
+'''string_list = ["a", "B", "C"]
+
+for i in range(len(string_list)):
+
+
+    string_list[i] = string_list[i].lower()
+Convert each string to lowercase
+
+
+print(string_list)'''
+
 a = ["foo", "bar", "baz"]
 
 y = []
-
+for i in range(len(a)):
+    a[i]=a[i].upper()
+    
+y.append(a) 
 print(y)
 
 # Use a list comprehension to create a list containing only the _even_ elements
